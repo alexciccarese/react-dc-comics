bc12e63df51c34042baccbf80a3c97b560f0efca
alexciccarese <alex.ciccarese@gmail.com>
2025-03-20 18:15:26 +0100

    enhance Main component layout with content and icons sections; update CSS for improved styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,7 +2,15 @@ export default function Main() {
 
   return (
     <main>
-      <h1>Content goes here</h1>
+      <div className="content">
+        <div className="container">
+          <h1>-- Content goes here --</h1>
+        </div>
+      </div>
+
+      <div className="icons">
+        <div className="container"></div>
+      </div>
     </main>
   )
 }
