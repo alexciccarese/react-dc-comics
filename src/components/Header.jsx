21b59a5c66ff3ba1bf0a535fff300eb63e1ff4ea
alexciccarese <alex.ciccarese@gmail.com>
2025-03-20 17:45:52 +0100

    update index.html title and add Google Fonts; enhance Header with navigation menu and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,9 +3,24 @@ export default function Header() {
   return (
     <header>
       <div className="container">
-        <div>
+        <div className="logo">
           <img src="/img/dc-logo.png" alt="logo" />
         </div>
+
+        <nav className="menu">
+          <ul>
+            <li><a href="">CHARACTERS</a></li>
+            <li><a href="">COMICS</a></li>
+            <li><a href="">MOVIES</a></li>
+            <li><a href="">TV</a></li>
+            <li><a href="">GAMES</a></li>
+            <li><a href="">COLLECTIBLES</a></li>
+            <li><a href="">VIDEO</a></li>
+            <li><a href="">FANS</a></li>
+            <li><a href="">NEWS</a></li>
+            <li><a href="">SHOP</a></li>
+          </ul>
+        </nav>
       </div>
     </header>
 
