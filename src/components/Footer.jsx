3e5fdb3a2651173872e8e0a73a316b1b99f10c29
alexciccarese <alex.ciccarese@gmail.com>
2025-03-21 14:20:27 +0100

    enhance Footer component with additional links and update CSS for improved layout and styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,76 @@
 export default function Footer() {
   // logica
-  
-  //template here
-    return (
-      <footer>
-        <img className="logo-bg" src="/img/dc-logo-bg.png" alt="" />
 
-        <div className="credits">
-          <div className="container"></div>
+  //template here
+  return (
+    <footer>
+      <img className="logo-bg" src="/img/dc-logo-bg.png" alt="" />
+
+      <div className="info">
+        <div className="container">
+          <div className="row">
+
+            <div className="links">
+
+              <ul>
+                <h3 className="title">DC COMICS</h3>
+                <li><a href="">Characters</a></li>
+                <li><a href="">Comics</a></li>
+                <li><a href="">Movies</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">Videos</a></li>
+                <li><a href="">News</a></li>
+              </ul>
+
+              <ul>
+                <h3 className="title">SHOP</h3>
+                <li><a href="">Shop DC</a></li>
+                <li><a href="">Shop DC Collectibles</a></li>
+              </ul>
+            </div>
+
+            <div className="links">
+
+              <ul>
+                <h3 className="title">DC</h3>
+                <li><a href="">Terms Of Use</a></li>
+                <li><a href="">Privacy Policy</a></li>
+                <li><a href="">Ad Choices</a></li>
+                <li><a href="">Advertising</a></li>
+                <li><a href="">Jobs</a></li>
+                <li><a href="">Subscriptions</a></li>
+                <li><a href="">Talent Workshops</a></li>
+                <li><a href="">CPSC Certificates</a></li>
+                <li><a href="">Ratings</a></li>
+                <li><a href="">Shop Help</a></li>
+                <li><a href="">Contact Us</a></li>
+              </ul>
+
+            </div>
+
+            <div className="links">
+
+              <ul>
+                <h3 className="title">SITES</h3>
+                <li><a href="">DC</a></li>
+                <li><a href="">MAD Magazine</a></li>
+                <li><a href="">DC Kids</a></li>
+                <li><a href="">DC Universe</a></li>
+                <li><a href="">DC Power Visa</a></li>
+              </ul>
+
+            </div>
+
+          </div>
         </div>
-      </footer>
-    )
-  }
+      </div>
+
+      <div className="credits">
+        <div className="container">
+          <div className="row">
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
