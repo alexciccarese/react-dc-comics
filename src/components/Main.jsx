59297c6a38193c52aca920a747866047f3d76b1b
alexciccarese <alex.ciccarese@gmail.com>
2025-03-21 17:33:10 +0100

    add CardList component and integrate it into Main; update Card component formatting and adjust jumbotron height in CSS

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,4 +1,5 @@
 import Icons from '../components/Icons'
+import CardList from './cardList'
 export default function Main() {
 
   return (
@@ -8,6 +9,12 @@ export default function Main() {
       <div className="content">
         <div className="container">
           <div className="title">CURRENT SERIES</div>
+
+
+          {/* inserire qui il componente della lista card */}
+          <CardList />
+
+
           <div className="btn">
             <button>LOAD MORE</button>
           </div>
--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,13 +1,13 @@
-export default function Card({image, title}) {
+export default function Card({ image, title }) {
 
   return (
     <>
-                  <div className="col">
-              <div className="card">
-                <img width = "300" src={image} alt="" />
-                <div>{title}</div>
-              </div>
-            </div>
+      <div className="col">
+        <div className="card">
+          <img width="300" src={image} alt="" />
+          <div>{title}</div>
+        </div>
+      </div>
     </>
   )
 }
