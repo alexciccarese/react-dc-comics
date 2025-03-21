bf6b88850cb93284c9f9b45189c5eb372256ccd9
alexciccarese <alex.ciccarese@gmail.com>
2025-03-21 13:12:08 +0100

    add Icon component with multiple items and update CSS for layout and styling

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,9 +1,48 @@
-exports default function Icon() {
+export default function Icon() {
 
   return (
     <>
       <div className="icons">
-        <div className="container"></div>
+        <div className="container">
+          <div className="row">
+
+            <div className="item">
+              <div className="imgBox">
+                <img src="/img/buy-comics-digital-comics.png" alt="" />
+              </div>
+              <p>DIGITAL COMICS</p>
+            </div>
+
+            <div className="item">
+              <div className="imgBox">
+                <img src="/img/buy-comics-merchandise.png" alt="" />
+              </div>
+              <p>DC MERCHANDISE</p>
+            </div>
+
+            <div className="item">
+              <div className="imgBox">
+                <img src="/img/buy-comics-subscriptions.png" alt="" />
+              </div>
+              <p>SUBSCRIPTION</p>
+            </div>
+
+            <div className="item">
+              <div className="imgBox">
+                <img src="/img/buy-comics-shop-locator.png" alt="" />
+              </div>
+              <p>COMIC SHOP LOCATOR</p>
+            </div>
+
+            <div className="item">
+              <div className="imgBox">
+                <img src="/img/buy-dc-power-visa.svg" alt="" />
+              </div>
+              <p>DC POWER VISA</p>
+            </div>
+
+          </div>
+        </div>
       </div>
     </>
   )
